4507bf1e1088a58fddc32383251ce175be55aa58
PaulJalagari <paul.jalagari@imaginea.com>
2015-10-12 12:20:17 +0530

    Initial Generic WebScraper

---
+++ src/main/java/crawler/java/Web/Crawler.java
@@ -0,0 +1,119 @@
+package crawler.java.Web;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+import crawler.java.pagemanager.PageManager;
+import crawler.java.robot.Robot;
+import crawler.java.thread.CrawlerThread;
+import crawler.java.url.GlobalURL;
+
+import java.io.*;
+ 
+public class Crawler implements Runnable {
+ 
+ 
+private String seed;
+private LinkedHashSet newLinks = new LinkedHashSet();//to preserve the order of insertion
+private HashSet oldLinks = new HashSet();
+private static ArrayList<HashSet> allLinks = new ArrayList<HashSet>();
+//this variable contains links having been found so far.
+private  static HashSet foundLinks = new HashSet();
+private CrawlerThread crawlerThread = new CrawlerThread();
+Thread t ;
+private static FileOutputStream output;
+private static PrintStream printer;
+ 
+public Crawler(String url){
+ 
+seed = url;
+t = new Thread(this);
+t.start();
+ 
+}
+public void run() {
+ 
+ 
+allLinks.add(newLinks);
+allLinks.add(oldLinks);
+//this variable stores root node
+ArrayList<String> links = new ArrayList<String>();
+PageManager m = new PageManager();
+Robot robotCheck = new Robot();
+GlobalURL global = new GlobalURL();
+ 
+try{
+ 
+output = new FileOutputStream("foundLinks.txt");
+printer = new PrintStream(output);
+crawlerThread.addToList(newLinks, seed);
+ 
+// main loop
+while(!newLinks.isEmpty()){
+ 
+ 
+String currentLink = crawlerThread.getNextLink(newLinks);
+ 
+URL u = new URL(currentLink);
+crawlerThread.addToList(oldLinks, currentLink);
+links = m.retrieveLinks(u);
+ 
+// for every links
+for(int i=0;i<links.size();i++){
+ 
+//check to see if link that we are looking for exists in the link list
+for(int h=0;h<global.getLink().size();h++){
+ 
+//System.out.println(global.getLink().get(h).toString());
+if(links.get(i).equals(global.getLink().get(h).toString()))
+{
+ 
+if(crawlerThread.addToList(foundLinks, currentLink)){
+ 
+System.out.println("Thread "+Thread.currentThread().getId()+": "+currentLink+" ---> "+global.getLink().get(h).toString());
+synchronized(printer){
+ 
+printer.append(currentLink+" ---> "+global.getLink().get(h).toString());
+printer.println();
+}
+ 
+}
+ 
+}
+ 
+}
+//check to see if links are new or not, and if they are new, insert into newLinks
+if(crawlerThread.isNewLink(allLinks, links.get(i).toString())){
+ 
+if(m.isVerified(links.get(i)) && robotCheck.robatAllowed(new URL(links.get(i)))){
+ 
+crawlerThread.addToList(newLinks, links.get(i));
+//System.out.print(crawlerThread.inc());
+//System.out.println(links.get(i)+"  " +Thread.currentThread().getId());
+ 
+}
+ 
+}
+ 
+}
+ 
+ 
+}
+ 
+ 
+}
+catch(IOException ex){
+ 
+System.out.println(ex);
+ 
+}
+ 
+ 
+ 
+}
+ 
+ 
+}
---
+++ src/main/java/crawler/java/webcrawler/WebCrawler.java
@@ -0,0 +1,157 @@
+package crawler.java.webcrawler;
+
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import crawler.java.Web.Crawler;
+import crawler.java.pagemanager.PageManager;
+import crawler.java.url.GlobalURL;
+ 
+public class WebCrawler {
+ 
+ 
+private String url;
+ 
+public static void main(String[] args)  {
+ 
+// TODO Auto-generated method stub
+ 
+System.out.println("Please enter link which you're looking for:");
+Scanner in = new Scanner(System.in);
+String url = in.nextLine();
+//this variable contains link which we are looking for.
+GlobalURL global = new GlobalURL();
+ 
+ 
+//long heapSize = Runtime.getRuntime().maxMemory();
+//System.out.println("Heap Size = " + heapSize); 
+try{
+ 
+ 
+File roots = new File("roots.txt");
+FileInputStream fis = new FileInputStream(roots);
+DataInputStream dis = new DataInputStream(fis);
+BufferedReader br = new BufferedReader(new InputStreamReader(dis));
+String line;
+ArrayList<String> links = new ArrayList<String>();
+PageManager pageManager = new PageManager();
+ 
+if(pageManager.isVerified(url)){
+ 
+//we add different variations of URL which we want to look for
+global.addLink(url);
+if(url.endsWith("/")){
+ 
+global.addLink(url.substring(0,url.length()-1));
+ 
+if(url.contains("www")){
+ 
+global.addLink("http://"+url.substring(11));
+global.addLink("http://"+url.substring(11,url.length()-1));
+ 
+}
+ 
+else{
+ 
+global.addLink("http://www."+url.substring(7));
+global.addLink("http://www."+url.substring(7,url.length()-1));
+ 
+}
+ 
+}
+else{
+ 
+global.addLink(url+"/");
+ 
+if(url.contains("www")){
+ 
+global.addLink("http://"+url.substring(11));
+global.addLink("http://"+url.substring(11)+"/");
+ 
+}
+ 
+else{
+ 
+global.addLink("http://www."+url.substring(7));
+global.addLink("http://www."+url.substring(7)+"/");
+ 
+}
+ 
+}
+ 
+URL x = new URL(url);
+String host = x.getHost();
+links.add("http://"+host);
+ 
+}
+else{
+ 
+System.out.println("Invalid URL");
+System.exit(0);
+ 
+}
+ 
+//add root nodes from file
+while((line = br.readLine()) != null){
+ 
+links.add(line);
+ 
+}
+ 
+//create thread for each root node
+for(int i=0;i<links.size();i++){
+ 
+Runnable crawl = new Crawler(links.get(i));
+//Thread t = new Thread(crawl);
+//t.start();
+ 
+/*try{
+ 
+t.join();
+ 
+}
+catch(Exception e){
+ 
+}*/
+ 
+ 
+}
+ 
+/*Runnable crawl1 = new Crawler("http://www.uwo.ca/");
+Thread t1 = new Thread(crawl1);
+ 
+Runnable crawl2 = new Crawler("http://www.utoronto.ca");
+Thread t2 = new Thread(crawl2);
+ 
+t1.start();
+    t2.start();
+ 
+    try{
+ 
+    t1.join();
+    t2.join();
+     
+}
+    catch(Exception e){
+ 
+     
+     
+}*/
+ 
+ 
+ 
+}
+catch(IOException ex){
+ 
+System.out.println(ex);
+ 
+}
+ 
+ 
+}
+ 
+ 
+ 
+}
